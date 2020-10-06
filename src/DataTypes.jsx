3c9e435dc78a7c598153d9ec641261185fd113f2
Bienfu <bairdd@oregonstate.edu>
2020-10-05 18:38:29 -0700

    added datatype for objects with name field

--- src/DataTypes.jsx
+++ src/DataTypes.jsx
@@ -5,7 +5,7 @@ export const DataTypes = [
     typeCheckFields: ["firstName", "lastName"],
     icon: "fa fa fa",
     display: ["firstName", "lastName"],
-    details: (chosen)=>(
+    details: (chosen) => (
       <div className="DetailDisplay">
         <div className="DetailDisplayContainer">
           {/* <div>User ID: {chosen.userId}</div> */}
@@ -23,22 +23,44 @@ export const DataTypes = [
     typeCheckFields: ["city", "street", "zipcode"],
     icon: "fa fa fa",
     display: ["city", "street", "zipcode"],
-    details: (chosen)=>(
-        <div className="DetailDisplay">
-          <div className="DetailDisplayContainer">
-            {/* <div>User ID: {chosen.userId}</div> */}
-            <label for="street">Street:</label>
-            <input type="text" id="street" name="street" value={chosen.street} />
-            <label for="city">City:</label>
-            <input type="text" id="city" name="city" value={chosen.city} />
-            <label for="state">State:</label>
-            <input type="text" id="state" name="state" value={chosen.state} />
-            <label for="zipcode">ZipCode:</label>
-            <input type="text" id="zipcode" name="zipcode" value={chosen.zipcode} />
-            <input className="Button" type="submit" value="Submit" />
-          </div>
+    details: (chosen) => (
+      <div className="DetailDisplay">
+        <div className="DetailDisplayContainer">
+          {/* <div>User ID: {chosen.userId}</div> */}
+          <label for="street">Street:</label>
+          <input type="text" id="street" name="street" value={chosen.street} />
+          <label for="city">City:</label>
+          <input type="text" id="city" name="city" value={chosen.city} />
+          <label for="state">State:</label>
+          <input type="text" id="state" name="state" value={chosen.state} />
+          <label for="zipcode">ZipCode:</label>
+          <input
+            type="text"
+            id="zipcode"
+            name="zipcode"
+            value={chosen.zipcode}
+          />
+          <input className="Button" type="submit" value="Submit" />
         </div>
-      ),
+      </div>
+    ),
+  },
+  {
+    typeName: "Name",
+    typeCheckFields: ["name"],
+    icon: "fa fa fa",
+    display: ["name"],
+    details: (chosen) => (
+      <div className="DetailDisplay">
+        <div className="DetailDisplayContainer">
+          {/* <div>User ID: {chosen.userId}</div> */}
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" value={chosen.name} />
+          {/* <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname" value={chosen.lastName} /> */}
+          <input className="Button" type="submit" value="Submit" />
+        </div>
+      </div>
+    ),
   },
 ];
-
